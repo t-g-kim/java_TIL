725bb47f679cbb42566d36d052340f562e0bb734
tgkim <je23ct@gmail.com>
2024-03-25 01:51:26 +0900

    study

---
+++ src/main/java/algorithm/do_it/Problem004.java
@@ -0,0 +1,6 @@
+package algorithm.do_it;
+
+// P67 007 DNA 비밀번호, 백준 12891
+public class Problem004 {
+  
+}
---
+++ src/main/java/algorithm/do_it/Programmers01.java
@@ -0,0 +1,20 @@
+package algorithm.do_it;
+
+import java.util.*;
+// https://school.programmers.co.kr/learn/courses/30/lessons/43238
+// 입국 심사
+// 해설 https://born2bedeveloper.tistory.com/40
+// https://velog.io/@lifeisbeautiful/Java-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9D%B4%EB%B6%84%ED%83%90%EC%83%89-%EC%9E%85%EA%B5%AD%EC%8B%AC%EC%82%AC-%EC%9E%90%EB%B0%94
+// https://youngest-programming.tistory.com/499
+public class Programmers01 {
+  public static void main(String[] args) {
+    int[] timeArr = {7, 10};
+    int n = 6;
+
+    Arrays.sort(timeArr);
+    long min = 1;
+    long max = (long) timeArr[timeArr.length - 1] * n;
+
+
+  }
+}
